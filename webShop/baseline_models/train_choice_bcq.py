271cbcd9e1ed16a7a218c611f583d6f4050e1990
Ishita Mediratta <ishitamediratta19@gmail.com>
2023-10-25 23:22:10 +0530

    Update train_choice_bcq.py

--- webShop/baseline_models/train_choice_bcq.py
+++ webShop/baseline_models/train_choice_bcq.py
@@ -1,8 +1,4 @@
 # Copyright (c) Meta Platforms, Inc. and affiliates.
-# All rights reserved.
-
-# This source code is licensed under the license found in the
-# LICENSE file in the root directory of this source tree.
 #
 # This file is modified from train_choice_il.py which is licensed as below to include specific code for BCQ model.
 #
