e7bb8a7a52a30625c4e4de9e098c8db788efce6f
Ishita Mediratta <ishitamediratta19@gmail.com>
2023-10-25 22:53:49 +0530

    Update __init__.py

--- procgen/online/behavior_policies/__init__.py
+++ procgen/online/behavior_policies/__init__.py
@@ -1,13 +1,6 @@
-# Copyright (c) 2017 Ilya Kostrikov
-# 
-# Licensed under the MIT License;
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     https://opensource.org/licenses/MIT
-#
-# This file is a modified version of:
-# https://github.com/rraileanu/auto-drac/blob/master/ucb_rl2_meta/arguments.py
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+# This source code is licensed under the license found in the LICENSE file in the root directory of this source tree.
 
 from .envs import VecPyTorchProcgen, make_venv
 from .model import PPOnet
