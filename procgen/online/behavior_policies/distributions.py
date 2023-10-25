221e5434f13370f59415533f1a858e7b96ae0719
Ishita Mediratta <ishitamediratta19@gmail.com>
2023-10-25 22:53:10 +0530

    Update distributions.py

--- procgen/online/behavior_policies/distributions.py
+++ procgen/online/behavior_policies/distributions.py
@@ -1,8 +1,13 @@
-# Copyright (c) Meta Platforms, Inc. and affiliates.
-# All rights reserved.
+# Copyright (c) 2017 Ilya Kostrikov
+# 
+# Licensed under the MIT License;
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-# This source code is licensed under the license found in the
-# LICENSE file in the root directory of this source tree.
+#     https://opensource.org/licenses/MIT
+#
+# This file is a modified version of:
+# https://github.com/rraileanu/auto-drac/blob/master/ucb_rl2_meta/arguments.py
 
 import torch
 import torch.nn as nn
