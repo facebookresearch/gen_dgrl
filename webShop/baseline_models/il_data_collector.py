d86dc2886c76286ddd735798d6d0643ce62d3a8b
Ishita Mediratta <ishitamediratta19@gmail.com>
2023-10-25 23:17:36 +0530

    Update il_data_collector.py

--- webShop/baseline_models/il_data_collector.py
+++ webShop/baseline_models/il_data_collector.py
@@ -3,6 +3,8 @@
 
 # This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
+#
+# This file is modified from test.py file
 
 import argparse
 import json
