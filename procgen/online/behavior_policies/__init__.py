1f4f2012b8571a28a0b340ac3a7e2f7aaec48274
Ishita Mediratta <ishitamediratta19@gmail.com>
2023-10-25 22:49:20 +0530

    Update __init__.py

--- procgen/online/behavior_policies/__init__.py
+++ procgen/online/behavior_policies/__init__.py
@@ -1,3 +1,9 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
 from .envs import VecPyTorchProcgen, make_venv
 from .model import PPOnet
 from .replay_buffer import ReplayBuffer
