10271acbff78ef46ea20660745973886708d2318
Ishita Mediratta <ishitamediratta19@gmail.com>
2023-10-25 22:50:17 +0530

    Update envs.py

--- procgen/online/behavior_policies/envs.py
+++ procgen/online/behavior_policies/envs.py
@@ -1,3 +1,9 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
 import procgen
 import torch
 from baselines.common.vec_env import VecEnvWrapper, VecExtractDictObs, VecMonitor, VecNormalize
