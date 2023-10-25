9e42497cae1eade3c643ad1b4604b84657c57860
Ishita Mediratta <ishitamediratta19@gmail.com>
2023-10-25 23:02:19 +0530

    Update get_ppo.py

--- procgen/online/get_ppo.py
+++ procgen/online/get_ppo.py
@@ -1,3 +1,9 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
 # Run PPO final checkpoints on Procgen test levels
 import argparse
 import os
