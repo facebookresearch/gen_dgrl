f9c0efa8a60e96ea1bf485b4f6de07239387c835
Ishita Mediratta <ishitamediratta19@gmail.com>
2023-10-25 23:21:56 +0530

    Update train_choice_cql.py

--- webShop/baseline_models/train_choice_cql.py
+++ webShop/baseline_models/train_choice_cql.py
@@ -1,10 +1,6 @@
 # Copyright (c) Meta Platforms, Inc. and affiliates.
-# All rights reserved.
-
-# This source code is licensed under the license found in the
-# LICENSE file in the root directory of this source tree.
 #
-# This file is modified from train_choice_il.py which is licensed as below to include specific code for BCQ model.
+# This file is modified from train_choice_il.py which is licensed as below to include specific code for CQL model.
 #
 # Copyright 2021 The HuggingFace Inc. team. All rights reserved.
 #
